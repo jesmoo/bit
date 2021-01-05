644a785a3302ba8bb15a72a789bf8ffaf4219b7d
jesmoo <habidmoo@gmail.com>
2021-01-04 19:10:45 -0500

    se agrego ejercicios que practico

--- js/alert.js
+++ js/alert.js
@@ -1,1 +1,36 @@
-// alert("Esta pagina no tiene fin comercial, es parte de PORTAFOLIO de Jesmoo. ");
+/* var miAuto = {
+    marca: "vento",
+    modelo: "tornado",
+    annio: 2021,
+    detallesDelAuto: function () {
+        console.log("auto: "+ this.modelo + " del año: " + this.annio );
+    }
+}; */
+
+/* function auto(marca, modelo, annio) {
+    this.marca = marca;
+    this.modelo = modelo;
+    this.annio = annio;    
+}
+
+let marca = ["judith","jesmoo","huesoAtenea"];
+let modelo = ["humano","humano","lomitosJugosos"];
+let annio = [2002,2002,2019];
+let cars = [];
+
+for (let i = 0; i < marca.length; i++) {
+    let newCars= new auto(marca[i],modelo[i],annio[i]);
+    cars.push(newCars);
+    console.log(newCars);
+} */
+
+var articulos = [
+    { nombre: "bici", costo: 3000 },
+    { nombre: "teclado", costo: 250 },
+    { nombre: "auto", costo: 30000 },
+    { nombre: "moto", costo: 32000 },
+    { nombre: "gasolina", costo: 150 },
+    { nombre: "dolar", costo: 20 },
+    { nombre: "euro", costo: 24 },
+];
+
